e3404788f8ffbeb33aa86ec4156f7ba9752ddd88
Vlad <vladdaking@gmail.com>
2021-03-04 12:07:05 +0300

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'main#index'
+  get 'main/index'
+  get 'main/help'
+  get 'main/contacts'
+  get 'main/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
