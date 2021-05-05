12b605f0ec7faa3e3b59f41a06232394401a168d
Vlad <vladdaking@gmail.com>
2021-05-05 21:44:48 +0300

    theme model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_184220) do
+ActiveRecord::Schema.define(version: 2021_05_05_184425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2021_05_05_184220) do
     t.string "file"
     t.string "ave_value"
     t.integer "theme_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "themes", force: :cascade do |t|
+    t.string "name"
+    t.integer "qty_items"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
