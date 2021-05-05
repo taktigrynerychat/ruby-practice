1aa1d495d3f275465b458c4e2fe7326000bffb1c
Vlad <vladdaking@gmail.com>
2021-05-06 01:07:06 +0300

    add chevrons and refactor image styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def cp(path)
     "current" if current_page?(path)
   end
+  #
+  # def cg(cur, sel)
+  #   logger.info "curr = #{cur}, sel = #{sel}"
+  #   "current" if cur == sel
+  # end
 end
