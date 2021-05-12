3af064017e0f095a86f521767e96b90a319929f8
Vlad <vladdaking@gmail.com>
2021-05-12 19:56:53 +0300

    add jquery and switching themes add star rating component

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   match 'work', to: 'work#index',via: 'get'
   match 'choose_image',     to: 'work#choose_image',      via: :get
   match 'choose_theme', to: 'work#choose_theme', via: :get
+  match 'rate', to: 'work#rate', via: :get
   match 'display_theme', to: 'work#display_theme', via: :post, as: :js
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
